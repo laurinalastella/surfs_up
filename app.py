39b4ff16c5f2bf3f8f65a2f8f2b91c04985b6261
Laurina LaStella <106928784+laurinalastella@users.noreply.github.com>
2022-08-07 21:03:34 -0700

    Mod 9 Flask route errors.

--- app.py
+++ app.py
@@ -1,63 +1,111 @@
-# DABC Module 9
-# 2022-08-07
-# LaStella
+# Mod 9.5.1
 
-# Create a New Python File and Import the Flask Dependency
-# Create a new Python file called app.py. You should create this file in VS Code.
-# Once the Python file is created, we can import the dependency we need. 
-# This dependency will enable your code to access all that Flask has to offer.
-# To import the Flask dependency, add the following to your code:
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
-# We're now ready to create a new Flask app instance. 
-# "Instance" is a general term in programming to refer to a 
-# singular version of something. Add the following to your 
-# code to create a new Flask instance called app:
+from flask import Flask, jsonify
+
+# The create_engine() function allows 
+# us to access and query our SQLite database file.
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect the database into our classes.
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# create a variable for each of the 
+# classes so that we can reference them later, as shown below.
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# create a session link from 
+# Python to our database with the following code:
+session = Session(engine)
+
+# To define our Flask app, add the following line of code. 
+# This will create a Flask application called "app."
 app = Flask(__name__)
-# This __name__ variable denotes the name of the current function. 
-# You can use the __name__ variable to determine if your code 
-# is being run from the command line or if it has been imported 
-# into another piece of code. Variables with underscores 
-# before and after them are called magic methods in Python.
-
-
-# First, we need to define the starting point, also known as 
-# the root. To do this, we'll use the function @app.route('/'). 
-# Add this to your code now.
-@app.route('/')
-# Notice the forward slash inside of the app.route? 
-# This denotes that we want to put our data at the 
-# root of our routes. The forward slash is commonly 
-# known as the highest level of hierarchy in any computer system.
-
-def hello_world():
-    return 'Hello world'
-
-#  Windows: Start by opening up Anaconda Powershell. 
-# Once you've done that, enter this command.
-    # set FLASK_APP=app.py
-# Now let's run our Flask app. To do this, type the 
-# following command in your command line and press Enter:
-    # flask run
-
-# When you run this command, you'll notice a line that says "Running on" followed by an address. This should be your localhost address and a port number.
+# Notice the __name__ variable in this code. This is 
+# a special type of variable in Python. Its value 
+# depends on where and how the code is run. For 
+# example, if we wanted to import our app.py file 
+# into another Python file named example.py, the 
+# variable __name__ would be set to example.
 
 # IMPORTANT
-# A port number is essentially the endpoint of a given program 
-# or service. Any Flask application you create can have 
-# whatever port number you would like, but the most common is 5000.
+# All of your routes should go after the 
+#     app = Flask(__name__) 
+# line of code. Otherwise, your code may not run properly.
+@app.route("/")
 
-# Copy and paste your localhost address into your web browser. 
-# Generally, a localhost will look something like this, 
-# for context. 
-# localhost:5000
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
 
-# http://127.0.0.1:5000/
+# NOTE
+# When creating routes, we follow the naming convention
+#    /api/v1.0/ 
+# followed by the name of the route. This convention 
+# signifies that this is version 1 of our application. 
+# This line can be updated to support future versions 
+# of the app as well.
+
+# The welcome route is now defined, so let's try to run our code. 
+# You can run Flask applications by using the command below, 
+# but you'll need a web browser to view the results.
+
+# Let's start by using the command line to navigate to 
+# your project folder. Then run your code:
+#   flask run
+# After starting the flask application, you'll likely 
+# see more text output than you have so far. This is 
+# exactly what should happen. The output will probably 
+# look like ... a web address where you can view your results:
+
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+    return
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   return
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+   return
+
+# Mod 9.5.3
+
+# "jsonify" our dictionary. Jsonify() is a function that 
+# converts the dictionary to a JSON file.
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+#    http://127.0.0.1:5000/api/v1.0/precipitation
+# Internal Server Error http://127.0.0.1:5000/api/v1.0/
 
 
-# New route test:
-# @app.route('/zebra')
 
 
 
